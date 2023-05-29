d9d3e0e831981b9db4c390fb2504f441c2e2bad8
WangSL <wangshilong0809@outlook.com>
2023-05-29 22:56:32 +0800

    Initial commit

---
+++ src/binary_search.go
@@ -0,0 +1,19 @@
+package src
+
+// https://leetcode.cn/problems/binary-search/
+func Search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	//mid := 0
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
---
+++ test/binary_search_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"programmercarl/src"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	nums := []int{1, 3, 5, 7, 9}
+	target := 3
+	expected := 1
+	result := src.Search(nums, target)
+	if result != expected {
+		t.Errorf("search(%v, %d) = %d; want %d", nums, target, result, expected)
+	}
+
+	nums = []int{-10, -5, 0, 1, 3, 5, 7, 9}
+	target = 5
+	expected = 5
+	result = src.Search(nums, target)
+	if result != expected {
+		t.Errorf("search(%v, %d) = %d; want %d", nums, target, result, expected)
+	}
+
+	nums = []int{2, 4, 6, 8, 10}
+	target = 7
+	expected = -1
+	result = src.Search(nums, target)
+	if result != expected {
+		t.Errorf("search(%v, %d) = %d; want %d", nums, target, result, expected)
+	}
+}
