5892f4979c2e91c92845c7f4488c35bc6e0223e8
WangSL <wangshilong0809@outlook.com>
2023-05-30 21:45:08 +0800

    RemoveElement

--- src/removeElement.go
+++ src/removeElement.go
@@ -3,12 +3,12 @@ package src
 // RemoveElement https://leetcode.cn/problems/remove-element/
 
 func RemoveElement(nums []int, val int) int {
-	low := 0
+	slow := 0
 	for fast := 0; fast < len(nums); fast++ {
 		if nums[fast] != val {
-			nums[low] = nums[fast]
-			low++
+			nums[slow] = nums[fast]
+			slow++
 		}
 	}
-	return low
+	return slow
 }
